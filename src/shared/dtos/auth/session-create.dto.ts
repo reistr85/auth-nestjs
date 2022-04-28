9ec458cb3b6fb8c2017e4f09bed486923ad48bc7
Ana Moraes <ana.moraes@usemobile.xyz>
2022-04-28 17:55:48 -0300

    docs: describe fields in dto

--- src/shared/dtos/auth/session-create.dto.ts
+++ src/shared/dtos/auth/session-create.dto.ts
@@ -5,6 +5,7 @@ export class SessionCreateDto {
     type: String,
     required: true,
     example: 'fulano@usemobile.xyz',
+    description: 'Email utilizado para fazer login no sistema',
   })
   email: string;
 
@@ -12,6 +13,7 @@ export class SessionCreateDto {
     type: String,
     required: true,
     example: 'strong_password',
+    description: 'Senha utilizada para fazer login no sistema',
   })
   password: string;
 }
