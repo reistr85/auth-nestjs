dfe815d4657e700911a6d34158611436a06fa4c2
Ana Moraes <ana.moraes@usemobile.xyz>
2022-04-28 15:46:45 -0300

    chore: complete session dto

--- src/shared/dtos/auth/session-create.dto.ts
+++ src/shared/dtos/auth/session-create.dto.ts
@@ -1,4 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class SessionCreateDto {
+  @ApiProperty({
+    type: String,
+    required: true,
+    example: 'fulano@usemobile.xyz',
+  })
   email: string;
+
+  @ApiProperty({
+    type: String,
+    required: true,
+    example: 'strong_password',
+  })
   password: string;
 }
--- src/shared/dtos/auth/session-created.dto.ts
+++ src/shared/dtos/auth/session-created.dto.ts
@@ -1,6 +1,15 @@
 import { UserEntity } from 'src/core/domain/entities/user.entity';
+import { Expose } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SessionCreatedDto {
+  @Expose()
+  @ApiProperty({ type: UserEntity })
   user: UserEntity;
+
+  @Expose()
+  @ApiProperty({
+    example: 'asd65as4d.ASDas1d351a3s51das.ass654',
+  })
   token: string;
 }
