66e280887a47215181dc8b998d10428c7f5b6ff5
Ana Moraes <ana.moraes@usemobile.xyz>
2022-04-28 18:01:36 -0300

    docs: include swagger in user controller

--- src/presentation/users.controller.ts
+++ src/presentation/users.controller.ts
@@ -3,8 +3,16 @@ import { UserEntity } from 'src/core/domain/entities/user.entity';
 import { UserCreateDto } from 'src/shared/dtos/auth/user-create.dto';
 import { CreateUserUseCase } from 'src/use-cases/auth/create-user.usecase';
 import { GetAllUsersUseCase } from 'src/use-cases/auth/get-all-users.usecase';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller('/users')
+@ApiBearerAuth()
 export class UsersController {
   constructor(
     private createUserUserCase: CreateUserUseCase,
@@ -12,11 +20,13 @@ export class UsersController {
   ) {}
 
   @Post()
+  @ApiBody({ type: UserCreateDto })
   public create(@Body() user: UserCreateDto) {
     return this.createUserUserCase.execute(user);
   }
 
   @Get()
+  @ApiOkResponse({ type: [UserEntity] })
   public findAll(): UserEntity[] {
     return this.getAllUsersUserCase.execute();
   }
