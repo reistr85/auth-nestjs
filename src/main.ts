fd4d1fcf324a898fc024cef5258eee2553042024
Ana Moraes <ana.moraes@usemobile.xyz>
2022-04-28 12:06:08 -0300

    feat: set swagger in application

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  DocumentBuilder,
+  SwaggerModule,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1/ampar');
-  await app.listen(3000);
+  setDocumentation(app);
+  app.enableCors();
+  await app.listen(Number(process.env.PORT));
 }
+
+const setDocumentation = (app) => {
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('Documentation app')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+    customSiteTitle: 'Docs',
+  });
+}
+
 bootstrap();
